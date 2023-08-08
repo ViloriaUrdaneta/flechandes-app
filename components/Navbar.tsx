31faced936ee03d0d870c9d4a17f8dfd87857941
Miguel <miguel.viloria.247@gmail.com>
2023-08-08 16:51:23 -0400

    pathname

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,37 @@
+'use client'
 import React from 'react'
+import { usePathname  } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Navbar() {
-  return (
-    <div>
-      <div className='fixed inset-x-0 top-0 bg-gray-100 w-screen h-14 flex items-center justify-center'>
-        <h2 className='font-bold text-lg text-black'>Inicio</h2>
-      </div>
-    </div>
-  )
+
+    const pathname = usePathname();
+
+    let location = ''
+
+    if(pathname === '/'){
+        location = 'Inicio'
+    }else if(pathname === '/publicar'){
+        location = 'Publicar'
+    }
+   
+
+    return (
+        <div>
+            <div className='fixed inset-x-0 top-0 bg-gray-100 w-screen h-14 flex items-center justify-around '>
+                { pathname === '/' ? (
+                    <h2 className='font-bold text-lg text-black mr-1'>{location}</h2>
+                ):(
+                    <>
+                        <Link href={'/'}>
+                            <p>i</p>
+                        </Link>
+                        <h2 className='font-bold text-lg text-black mr-1'>{location}</h2>
+                        <p></p>
+                    </>
+                )}
+                
+            </div>
+        </div>
+    )
 }
--- components/Tapbar.tsx
+++ components/Tapbar.tsx
@@ -1,34 +1,39 @@
+'use client'
 import React from 'react'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Tapbar() {
-  return (
-    <div>
-      <div className='fixed bottom-0 right-0 bg-red w-screen h-20 flex items-center justify-evenly text-white'>
-        <Link href={'/'}>
-            <div className='text-center'>
-                <h1>i</h1>
-                <p className='text-xs'>
-                    Inicio
-                </p>
+
+    const pathname = usePathname()
+
+    return (
+        <div>
+            <div className='fixed bottom-0 right-0 bg-red w-screen h-20 flex items-center justify-evenly text-white'>
+                <Link href={'/'}>
+                    <div className='text-center'>
+                        <h1>i</h1>
+                        <p className='text-xs'>
+                            Inicio
+                        </p>
+                    </div>
+                </Link>
+                <Link href={'/publicar'}>
+                    <div className='text-center'>
+                        <h1>i</h1>
+                        <p className='text-xs'>
+                            Publicar
+                        </p>
+                    </div>
+                </Link>
+                
+                <div className='text-center'>
+                    <h1>i</h1>
+                    <p className='text-xs'>
+                        Perfil
+                    </p>
+                </div>
             </div>
-        </Link>
-        <Link href={'/publicar'}>
-            <div className='text-center'>
-                <h1>i</h1>
-                <p className='text-xs'>
-                    Publicar
-                </p>
-            </div>
-        </Link>
-        
-        <div className='text-center'>
-            <h1>i</h1>
-            <p className='text-xs'>
-                Perfil
-            </p>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
